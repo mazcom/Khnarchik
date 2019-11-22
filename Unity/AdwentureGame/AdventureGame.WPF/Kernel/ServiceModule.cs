d4fc9242b4ba46ff00fe9b1fad9430bc0c26a87a
mazcom@mail.ru <mazcom@mail.ru>
2019-11-22 21:47:10 +0200

    clean code

--- Unity/AdwentureGame/AdventureGame.WPF/Kernel/ServiceModule.cs
+++ Unity/AdwentureGame/AdventureGame.WPF/Kernel/ServiceModule.cs
@@ -1,11 +1,6 @@
 ﻿using AdventureGame.Domain;
 using AdventureGame.Infrastructure;
 using Ninject.Modules;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AdventureGame.WPF.Kernel {
 
@@ -13,7 +8,6 @@ namespace AdventureGame.WPF.Kernel {
 
     public override void Load() {
 
-      //Bind<IStateRepository>().To<JsonStateRepository>().InTransientScope();
       Bind<IStateRepository>().To<FakeStateRepository>().InTransientScope();
     }
   }
