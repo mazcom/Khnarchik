4be77830c4b41f78ebdd0fa322564517da99fb9c
vitaliiz <vitaliiz>
2022-10-15 22:05:45 +0300

    add EmbeddedFileProvider

---
+++ EmbeddedResources/json/Product.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace json
+{
+  
+  public class Product
+  {
+
+    [JsonPropertyName("productId")]
+    public int ProductId { get; set; }
+
+    [JsonPropertyName("productName")]
+    public string? ProductName { get; set; }
+
+    [JsonPropertyName("productPrice")]
+    [JsonConverter (typeof(DecimalJsonConverter))]
+    public decimal ProductPrice { get; set; }
+    
+  }
+
+  public class DecimalJsonConverter : JsonConverter<decimal>
+  {
+    public override decimal Read(
+        ref Utf8JsonReader reader,
+        Type typeToConvert,
+        JsonSerializerOptions options) =>
+            decimal.Parse(reader.GetString()!, CultureInfo.InvariantCulture);
+
+    public override void Write(
+        Utf8JsonWriter writer,
+        decimal dateTimeValue,
+        JsonSerializerOptions options) =>  dateTimeValue.ToString(CultureInfo.InvariantCulture);
+  }
+
+  public class DecimalJsonConverter2 : JsonConverter<decimal>
+  {
+    public override decimal Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+      throw new NotImplementedException();
+    }
+
+    public override void Write(Utf8JsonWriter writer, decimal value, JsonSerializerOptions options)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
---
+++ EmbeddedResources/json/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// https://learn.microsoft.com/en-us/dotnet/standard/serialization/system-text-json/converters-how-to?pivots=dotnet-6-0
+
+using json;
+using Microsoft.Extensions.FileProviders;
+using System.Reflection;
+using System.Text.Json;
+
+//Console.WriteLine("Hello, World!");
+
+// https://josef.codes/using-embedded-files-in-dotnet-core/
+
+
+var prov = new EmbeddedFileProvider(Assembly.GetExecutingAssembly());
+var stream = prov.GetFileInfo("products.json").CreateReadStream();
+
+string allData;
+using (StreamReader reader = new StreamReader(stream))
+{
+  allData = reader.ReadToEnd();
+}
+
+//allData = File.ReadAllText("products.json");
+
+var options = new JsonSerializerOptions();
+options.Converters.Add(new DecimalJsonConverter());
+
+
+var products =
+                JsonSerializer.Deserialize<List<Product>>(allData, options);
+
+
+Console.WriteLine("End of program");
+
+
+
+
